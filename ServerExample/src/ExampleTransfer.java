c3b22f35c5e49ae11140ddc86b4bf322156a1c98
TheCatLover <johnthejolly@gmail.com>
2017-09-28 18:02:51 -0500

    Add files via upload

---
+++ ServerExample/src/ExampleTransfer.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import framework.logging.Logger;
+import framework.logging.MessageLevels;
+import framework.transfer.Transfer;
+
+public class ExampleTransfer {
+	
+}
---
+++ ServerExample/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+import framework.logging.Logger;
+import framework.logging.MessageLevels;
+import framework.server.ServerFramework;
+import framework.transfer.DataTransfers;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+	}
+
+}
