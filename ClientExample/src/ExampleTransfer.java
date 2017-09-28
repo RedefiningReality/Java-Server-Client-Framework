edca1502150fe516ab91ec98f214ddc7c328ac80
TheCatLover <johnthejolly@gmail.com>
2017-09-28 18:03:16 -0500

    Add files via upload

---
+++ ClientExample/src/ExampleTransfer.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import framework.properties.PropertiesFile;
+import framework.transfer.Transfer;
+import gui.dialog.basic.input.TextInputDialog;
+import gui.dialog.userinput.PropertiesDialog;
+
+public class ExampleTransfer {
+	
+}
---
+++ ClientExample/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+import framework.logging.Logger;
+import framework.logging.MessageLevels;
+import framework.transfer.DataTransfers;
+import gui.client.ClientGUI;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+	}
+
+}
