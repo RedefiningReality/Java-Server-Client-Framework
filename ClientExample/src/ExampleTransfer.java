d9efbad2ce7db37d0ac76019101cfff890e0fd89
TheCatLover <johnthejolly@gmail.com>
2017-09-28 18:07:14 -0500

    Add files via upload

--- ClientExample/src/ExampleTransfer.java
+++ ClientExample/src/ExampleTransfer.java
@@ -7,6 +7,14 @@ import framework.transfer.Transfer;
 import gui.dialog.basic.input.TextInputDialog;
 import gui.dialog.userinput.PropertiesDialog;
 
-public class ExampleTransfer {
+public class ExampleTransfer extends Transfer {
+
+	@Override
+	public void start(BufferedReader in, PrintWriter out) throws IOException {
+		TextInputDialog textInput = new TextInputDialog("Please enter a word: ");
+		textInput.setDefaultValue(PropertiesFile.getProperty("word"));
+		String word = new PropertiesDialog(textInput, "word").start();
+		out.println(word);
+	}
 	
 }
--- ClientExample/src/Main.java
+++ ClientExample/src/Main.java
@@ -8,7 +8,11 @@ import gui.client.ClientGUI;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+		Logger.setLogFile("log.txt");
 		
+		DataTransfers.addTransfer("Example", new ExampleTransfer());
+		ClientGUI.initialize("localhost", 12345, true, MessageLevels.DATA);
+		ClientGUI.run();
 	}
 
 }
